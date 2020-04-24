831d073f18b5fb8cd83a99b40dbbf5a00cf3f9f3
eservigon1 <ernestoservigon@gmail.com>
2020-04-24 22:37:22 +0000

    cambio a metodo1
    metodo1

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from student import student
 
 
 def crea_estudiantes(count_estudiantes):
-	print ("metodo vacio")
+	count = 0
+	list_students = []
+	
+	while count < int(count_estudiantes):
+
+		student_code = input("dame la matricula: ")
+		student_name = input("dame el nombre: ")
+		student_age = input("dame la edad: ")
+		student_gender = input("dame el genero: ")
+		student_carreer = input("dame la carrera: ")
+		
+		list_students.append(student(student_code,student_name,student_age,student_gender,student_carreer))
+		
+		count = count + 1
+	return list_students
+
 
 def ordena_edades(list_students):
 	print ("metodo vacio")
